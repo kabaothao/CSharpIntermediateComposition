99be06899a1afd6445f08b9cf658ffe933647faa
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-03-02 09:05:35 -0600

    Add project files.

---
+++ CSharpIntermediateComposition/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIntermediateComposition
+{
+    private readonly Logger _logger;
+    public class DbMigrator(Logger logger)
+    {
+        _logger = logger;
+
+    }
+
+
+    public void Migrate()
+
+
+}
